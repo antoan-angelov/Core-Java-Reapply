87b2cfdffda5e05c7212cf3dab071c3829f021c9
antoan-angelov <antoan.angelov@gmail.com>
2014-10-02 21:34:53 +0300

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 Core-Java-Reapply
 =================
 
-For HackBulgaria Android course.
+A word/line/character count utility written in Java for HackBulgaria Android course.
+
+This is an Eclipse project.  
+
+It uses the org.json library to format the result when not specified what to count (lines, words or characters).
+
+In order to run, simply navigate to /jar/wc.jar from the terminal and then run
+$ java -jar wc.jar [path-to-txt-file] [-c/-w/-l]
+
+Where path-to-txt-file is the path to the text file to be scanned, -c is for counting characters, -w for counting words and -l for counting lines, or leave the last argument empty to count all.
---
+++ src/com/antoan/reapply/WordCount.java
@@ -0,0 +1,107 @@
+package com.antoan.reapply;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.naming.directory.InvalidAttributesException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WordCount {
+
+	private final static String ARG_LINES = "-l";
+	private final static String ARG_WORDS = "-w";
+	private final static String ARG_CHARACTERS = "-c";
+
+	private static class CountResult {
+		public int characters = 0;
+		public int words = 0;
+		public int lines = 0;
+	}
+	
+	private static CountResult countEverything(File file) {
+		
+		CountResult result = new CountResult();
+		
+		try {
+			FileReader in = new FileReader(file);
+			BufferedReader br = new BufferedReader(in);
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				result.lines += 1;
+				result.characters += line.length();
+				if(!line.trim().isEmpty()) {
+					String[] words = line.split("([\\s]+)"); // split line by whitespace characters
+					result.words += words.length;
+				}
+			}
+			result.characters += result.lines - 1; // including the newline characters in character count too
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+
+	/**
+	 * Counts the words, lines, characters or all from a text file.
+	 * 
+	 * @param args First argument must always be the text file location.<br /> Second one can be either:<br/>
+	 * <ul>
+	 * 		<li>-l to count lines</li>
+	 * 		<li>-c to count characters</li>
+	 * 		<li>-w to count words</li>
+	 * 		<li>leave empty to count them all</li>
+	 * </ul>
+	 * @throws InvalidAttributesException If the text file is not specified or does not exist
+	 * @throws JSONException 
+	 */
+	public static void main(String[] args) throws InvalidAttributesException, JSONException {
+
+		if (args.length == 0) {
+			throw new InvalidAttributesException(
+					"Text file location is not specified as first parameter.");
+		}
+
+		String textFileLocation = args[0];
+		File textFile = new File(textFileLocation);
+
+		if (!textFile.exists()) {
+			throw new InvalidAttributesException("Text file does not exist.");
+		}
+		
+		CountResult result = countEverything(textFile);
+
+		if (args.length > 1) {
+			String countWhat = args[1];
+			switch (countWhat) {
+			case ARG_LINES:
+				System.out.println(result.lines);
+				break;
+			case ARG_CHARACTERS:
+				System.out.println(result.characters);
+				break;
+			case ARG_WORDS:
+				System.out.println(result.words);
+				break;
+			default:
+				throw new InvalidAttributesException("Invalid attribute " + args[1]);
+			}
+		}
+		else {
+			JSONObject obj = new JSONObject();
+			obj.put("lines", result.lines);
+			obj.put("words", result.words);
+			obj.put("chars", result.characters);			
+			System.out.println(obj.toString(2));
+		}
+	}
+
+}
